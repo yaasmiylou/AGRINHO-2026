f756e403e214e22239581582d9bb11c8340bee24
yaasmiylou <yasmin.irineu.lourenco@escola.pr.gov.br>
2026-07-02 12:09:59 -0300

    Add basic script for Agrinho 2026 project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script básico para o projeto Agrinho 2026
+
+// Exemplo: mensagem de boas-vindas no console
+console.log("Bem-vindo ao Projeto Agrinho 2026!");
+
+// Exemplo: alerta ao carregar a página
+window.onload = function() {
+    alert("Seja bem-vindo ao site do Agrinho 2026!");
+};
